70b6921a666a2f91517febdfb962bf01e95c3735
maticoul <maticoul@live.fr>
2023-09-30 13:39:40 +0000

    upgrade major for aws script

--- terraform/aws/to_sero/terraform/k8s_controllers.tf
+++ terraform/aws/to_sero/terraform/k8s_controllers.tf
@@ -3,7 +3,6 @@
 ############################
 
 resource "aws_instance" "controller" {
-
     count = 3
     ami = "${lookup(var.amis, var.region)}"
     instance_type = "${var.controller_instance_type}"
