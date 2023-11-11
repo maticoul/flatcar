bd5785bc8553fbae048b05ca12e1a7dcae3b0c1a
maticoul <maticoul@live.fr>
2023-11-11 12:21:28 +0000

    create IAM policies and roles for the csi-driver-iam

--- terraform/aws/multi-private-and-public-subnet-v3/infra/iam.tf
+++ terraform/aws/multi-private-and-public-subnet-v3/infra/iam.tf
@@ -61,3 +61,65 @@ resource  "aws_iam_instance_profile" "kube" {
  name = "kube"
  role = "${aws_iam_role.kube.name}" 
 }
+
+
+## create IAM policies and roles for the "csi-driver-iam
+
+
+resource "aws_iam_policy" "csi_driver_iam_policy" {
+  name        = "csi-driver-iam-policy"
+  description = "IAM policy for csi-driver-iam"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteTags",
+        "ec2:DeleteVolume",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeVolumeAttribute",
+        "ec2:DescribeVolumeStatus",
+        "ec2:DetachVolume",
+        "ec2:ModifyVolume",
+        "ec2:ResetSnapshotAttribute",
+        "ec2:ResetVolumeAttribute",
+        "ec2:DescribeVolumeAttribute"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "csi_driver_iam_role" {
+  name               = "csi-driver-iam-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "csi_driver_iam_attachment" {
+  policy_arn = aws_iam_policy.csi_driver_iam_policy.arn
+  role       = aws_iam_role.csi_driver_iam_role.name
+}
